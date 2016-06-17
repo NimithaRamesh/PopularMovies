2b183524df16a6c28333504e8018eb3002e52db1
Nimitha Ramesh <nimitha.r.ramesh@gmail.com>
2016-06-17 13:18:35 -0700

    Added ScrollView to DetailActivity

--- app/src/main/java/com/nimitharamesh/popularmovies/SettingsActivity.java
+++ app/src/main/java/com/nimitharamesh/popularmovies/SettingsActivity.java
@@ -1,11 +1,8 @@
 package com.nimitharamesh.popularmovies;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 /**
@@ -28,28 +25,6 @@ public class SettingsActivity extends AppCompatActivity /* implements Preference
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public static class SettingsFragment extends PreferenceFragment {
